dbc4750a7a4110e9477ae8a84acac966f2ceb81e
Adam4ever <adam4ever@gmail.com>
2024-12-15 09:20:21 -0400

    - SEO testing

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Adam4ever - the blogroject',
+  meta: [
+    {
+      name: 'description',
+      content: 'Adam4ever - the blogroject',
+    },
+  ],
+});
+</script>
 <template>
   <main>
     <ContentDoc />
