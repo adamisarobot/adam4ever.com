0e720d004b397cc1e7b2e7c8016f8b1246f509b0
Adam4ever <adam4ever@gmail.com>
2025-01-14 21:34:08 -0400

    Create SoonTM Component
    - Basic text component
    - Tried out using components in MD files

---
+++ components/content/SoonTm.vue
@@ -0,0 +1,20 @@
+<template>
+  <a>
+    <span data-soon>soon</span>
+    <span inert role="tooltip" class="tool-tip">
+      <span v-if="props.date">Completed: <br /></span> {{ message }}</span
+    >
+  </a>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  date: {
+    type: String,
+  },
+});
+
+const message = props.date ? props.date : 'Soon-ish? Maybe? :D';
+</script>
+
+<style scoped></style>
